bf0cb7ca566cc3d5ce0130d795ff45c9b14f45aa
Linux <adarsh00502@gmail.com>
2022-07-31 13:46:07 +0530

    add backend endpoints done

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,93 @@
 const express = require('express');
 const router = express.Router();
+const fetchuser = require('../middleware/fetchuser')
 
-router.get('/', (req, res)=>{
-    
-    res.json([])
-} )
+const Note = require('../models/Note')
+
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id });
+        res.json(notes)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+
+router.post('/addnote', fetchuser, async (req, res) => {
+    try {
+
+        const { title, description, tag } = req.body;
+
+
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save()
+
+        res.json(savedNote)
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+
+    const { title, description, tag } = req.body;
+    try {
+
+
+        // ceate a newNote object
+        const newNote = {};
+        if (title) { newNote.title = title }
+        if (description) { newNote.description = description }
+        if (tag) { newNote.tag = tag };
+
+        // find the note to be update
+
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not found") };
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Note Allowd")
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+
+    const { title, description, tag } = req.body;
+
+    try {
+        // find the note to be deleted
+
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not found") };
+
+        // allow deletion only if user owns this Note
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Note Allowd")
+        }
+
+        note = await Note.findByIdAndDelete(req.params.id)
+        res.json({ "success": "Note has been deleted", note: note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+
+
 
 module.exports = router
